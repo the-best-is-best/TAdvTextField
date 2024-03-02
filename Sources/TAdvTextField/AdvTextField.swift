0a715147dae0369bae84d2807a999067c9ab00db
Michelle <michelle.raouf@outlook.com>
2024-03-02 20:11:23 +0200

    fix

--- Sources/TAdvTextField/AdvTextField.swift
+++ Sources/TAdvTextField/AdvTextField.swift
@@ -6,15 +6,25 @@ public struct AdvTextField: View {
     public  var image: String? = nil
     public  var placeHolder: String
     public  var cornerRadius: Double = 12
-    @Binding public var value: String
+     public var value: String
     public  var fontSize: CGFloat = 20
     public  var fontWeight: Font.Weight = Font.Weight.regular
     public var onSubmit: () -> Void
-    
+    @State private var internalValue: String = ""
+    public init(image: String? = nil, placeHolder: String, cornerRadius: Double, value: String, fontSize: CGFloat, fontWeight: Font.Weight, onSubmit: @escaping () -> Void) {
+        self.image = image
+        self.placeHolder = placeHolder
+        self.cornerRadius = cornerRadius
+        self.value = value
+        self.fontSize = fontSize
+        self.fontWeight = fontWeight
+        self.onSubmit = onSubmit
+        internalValue = value
+    }
 
   public var body: some View {
     ZStack(alignment: Alignment(horizontal: .leading, vertical: .center)) {
-      TextField(placeHolder, text: $value) // Use public initializer
+      TextField(placeHolder, text: $internalValue) // Use public initializer
         .frame(height: 60)
         .padding(.horizontal, 75)
         .font(.system(size: fontSize, weight: fontWeight))
